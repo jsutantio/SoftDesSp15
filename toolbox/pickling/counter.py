2163681ce9063b1aec42303c5dd609ac6bb04ef5
jsutantio <jessica.sutantio@students.olin.edu>
2015-03-29 19:37:02 -0400

    first try

--- toolbox/pickling/counter.py
+++ toolbox/pickling/counter.py
@@ -15,7 +15,7 @@ def update_counter(file_name, reset=False):
 
 		file_name: the file that stores the counter to be incremented.  If the file
 				   doesn't exist, a counter is created and initialized to 1.
-		reset: True if the counter in the file should be rest.
+		reset: True if the counter in the file should be reset.
 		returns: the new counter value
 
 	>>> update_counter('blah.txt',True)
@@ -29,7 +29,17 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+	
+	if exists(file_name):
+		current_number = open(file_name, 'r+').read()
+		updated_number = int(current_number) + 1
+		existing_file = open(file_name, 'w')
+		return existing_file.write(str(updated_number))
+
+	else:
+		new_file = open(file_name, 'w')
+		return new_file.write('1')
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
