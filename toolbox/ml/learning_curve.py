cd7af5bc1681318630ead3a8a527e089d2d040bf
jsutantio <jessica.sutantio@students.olin.edu>
2015-03-30 02:00:12 -0400

    Added pictures and answers to questions.

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -8,22 +8,28 @@ from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
 # print data.DESCR
-num_trials = 10
+num_trials = 500
 train_percentages = range(5,90,5)
 test_accuracies = numpy.zeros(len(train_percentages))
-
-train_size = 0.5
+train_size = 0
+train_list = []
+test_list = []
 
 for i in range(num_trials):
 	for i in train_percentages:
 		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=i)
-		model = LogisticRegression(C=10**-10)
+		model = LogisticRegression(C=1**-10)
 		model.fit(X_train, y_train)
-		print "Train accuracy %f" %model.score(X_train,y_train)
-		print "Test accuracy %f"%model.score(X_test,y_test)
+		train_list.append(model.score(X_train,y_train))
+		test_list.append(model.score(X_test,y_test))
+		# print "Train accuracy %f" %model.score(X_train,y_train)
+		# print "Test accuracy %f"%model.score(X_test,y_test)
+# print train_list
+# print test_list
 
 fig = plt.figure()
-plt.plot(model.score(X_train,y_train), model.score(X_test,y_test))
+# plt.plot(train_list, test_list)
+plt.plot(train_list, test_list, linewidth=0, marker='.')
 plt.xlabel('Percentage of Data Used for Training')
 plt.ylabel('Accuracy on Test Set')
 plt.show()
