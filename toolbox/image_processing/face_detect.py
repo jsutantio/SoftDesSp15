39b1d46977eddfe9b80908c82a345499beed4063
jsutantio <jessica.sutantio@students.olin.edu>
2015-03-30 01:02:00 -0400

    made it run through num_trials. need to plot

--- toolbox/image_processing/face_detect.py
+++ toolbox/image_processing/face_detect.py
@@ -2,3 +2,22 @@
 
 import cv2
 import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+print cap.isOpened()
+cap.open(0)
+print cap.isOpened()
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()	
--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -7,20 +7,23 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 data = load_digits()
-print data.DESCR
+# print data.DESCR
 num_trials = 10
-train_percentages = range(5,95,5)
+train_percentages = range(5,90,5)
 test_accuracies = numpy.zeros(len(train_percentages))
 
-# train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
-# the resultant accuracy.
-# You should repeat each training percentage num_trials times to smooth out variability
-# for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
+train_size = 0.5
 
-# TODO: your code here
+for i in range(num_trials):
+	for i in train_percentages:
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=i)
+		model = LogisticRegression(C=10**-10)
+		model.fit(X_train, y_train)
+		print "Train accuracy %f" %model.score(X_train,y_train)
+		print "Test accuracy %f"%model.score(X_test,y_test)
 
 fig = plt.figure()
-plt.plot(train_percentages, test_accuracies)
+plt.plot(model.score(X_train,y_train), model.score(X_test,y_test))
 plt.xlabel('Percentage of Data Used for Training')
 plt.ylabel('Accuracy on Test Set')
 plt.show()
